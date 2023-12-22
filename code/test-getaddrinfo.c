09dbf58389955c83624bf027fa41dd890f9a4499
01musician <57641609@qq.com>
2023-12-22 08:29:15 +0800

    change getaddrinfo

--- code/test-getaddrinfo.c
+++ code/test-getaddrinfo.c
@@ -15,9 +15,11 @@ int main() {
 
     struct addrinfo hints, *res, *p;
     int status;
+    char ip_str[INET_ADDRSTRLEN];
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET; // Use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
 
     if ((status = getaddrinfo(hostname, NULL, &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
@@ -29,8 +31,7 @@ int main() {
     for (p = res; p != NULL; p = p->ai_next) {
         if (p->ai_family == AF_INET) { // Filter only IPv4 addresses
             struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-            char ip_str[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &(ipv4->sin_addr), ip_str, INET_ADDRSTRLEN);
+            inet_aton(ip_str, &(ipv4->sin_addr));
             printf("%s\n", ip_str);
         }
     }
