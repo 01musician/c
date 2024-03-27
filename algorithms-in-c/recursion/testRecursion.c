67a1698fc6a0ef65af75f64b83e604525cec0b4f
01musician <57641609@qq.com>
2024-03-27 10:16:17 +0800

    algorithm in c: add gcd recurrsion algorithm.

--- algorithms-in-c/recursion/testRecursion.c
+++ algorithms-in-c/recursion/testRecursion.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include "factorial.h"
 #include "fibonacci.h"
+#include "gcd.h"
 
 int main(int argc, char **argv)
 {
@@ -13,6 +14,8 @@ int main(int argc, char **argv)
     printf("fibonacci(10) is: %lld. \n", fibonacci(10));
     printf("fibonacci(20) is: %lld. \n", fibonacci(20));
     printf("fibonacci(50) is: %lld. \n", fibonacci(50));
+    int i = 314159, j = 271828;
+    printf("gcd of %d and %d is: %d. \n", i, j, gcd(i, j));
 
     return 0;
 }
