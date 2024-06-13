e6e736f8046872782ee43ffd5658ce17b3e27944
01musician <57641609@qq.com>
2024-06-13 16:41:29 +0800

    add publisher and subscriber which can communicate across machine. but still have to start publisher first.

---
+++ c-nng/remote_subscriber.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <nng/nng.h>
+#include <nng/protocol/pubsub0/sub.h>
+
+#define URL "tcp://127.0.0.1:5555" // Replace <publisher_ip> with the publisher's IP address
+
+void fatal(const char *func, int rv) {
+    fprintf(stderr, "%s: %s\n", func, nng_strerror(rv));
+    exit(1);
+}
+
+int main() {
+    nng_socket sock;
+    int rv;
+
+    if ((rv = nng_sub0_open(&sock)) != 0) {
+        fatal("nng_sub0_open", rv);
+    }
+
+    if ((rv = nng_dial(sock, URL, NULL, 0)) != 0) {
+        fatal("nng_dial", rv);
+    }
+
+    if ((rv = nng_setopt(sock, NNG_OPT_SUB_SUBSCRIBE, "", 0)) != 0) {
+        fatal("nng_setopt", rv);
+    }
+
+    while (1) {
+        char *buf = NULL;
+        size_t sz;
+
+        if ((rv = nng_recv(sock, &buf, &sz, NNG_FLAG_ALLOC)) != 0) {
+            fatal("nng_recv", rv);
+        }
+
+        printf("Received: %s\n", buf);
+        nng_free(buf, sz);
+    }
+
+    nng_close(sock);
+    return 0;
+}
+
